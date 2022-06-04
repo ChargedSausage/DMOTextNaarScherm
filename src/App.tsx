bebedf1e9d3a891c83eb6f2f81564a6d8a96eb1c
chargedsausage <chargedsausage@gmail.com>
2022-06-04 05:10:32 +0200

    Added settings page

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,32 @@
-import { Component, createMemo, createSignal, For } from 'solid-js'
+import { Component, createEffect, createMemo, createSignal, For, Show } from 'solid-js'
 
 import logo from './logo.svg'
 import styles from './App.module.css'
 
 type ResultScreenData = {
     text?: string
-    backgroundClass?: string
+    hex?: string
 }
 
 type Word = {
     word: string
-    backgroundClass: string
+    hex?: string
     text?: string
 }
 
 type WordToScreenData = {
     words: Word[]
-    foutBackgroundClass: string
+    foutHex?: string
     foutText?: string
 }
 
 const TextInput: Component<{
     value: string
     onSubmit: (value: string) => void
-    onInputChange: (event: InputEvent) => void
+    onInputChange: (event: InputEvent & {
+        currentTarget: HTMLInputElement;
+        target: Element;
+    }) => void
 }> = (props) => {
     const [word, setWord] = createSignal(props.value)
 
@@ -56,46 +59,84 @@ const ResultScreen: Component<{ data?: ResultScreenData; customDelay?: number; o
     )
 
     return (
-        <div class={styles.result}>
+        <div class={styles.result} style={{background: props.data?.hex}}>
             <h1>{props.data?.text || ''}</h1>
         </div>
     )
 }
 
-const Settings: Component = () => {
+const Settings: Component<{ data: WordToScreenData; onChange: (data: WordToScreenData) => void; onClose: () => void}> = (props) => {
+
     return (
         <div class={styles.curtain}>
-            <div class={styles.settings}></div>
+            <div class={styles.settings}>
+                <div class={styles.header}>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-settings" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" />
+                        <circle cx="12" cy="12" r="3" />
+                    </svg>
+                    <h1>Settings</h1>
+                    <button onClick={props.onClose}>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-settings" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                            <path d="M2 2l20 20" />
+                            <path d="M2 22l20 -20" />
+                        </svg>
+                    </button>
+                </div>
+                <div class={styles.table}>
+                    <div class={styles.mandatory}><b>Word</b></div>
+                    <div class={styles.mandatory}><b>Color</b></div>
+                    <div><b>Display Text</b></div>
+                    <div><b>Fout</b></div>
+                    <div><input type='color' value={props.data.foutHex} onChange={(e) => {
+                        const data = {...props.data};
+                        data.foutHex = e.currentTarget.value;
+                        props.onChange(data);
+                    }}/></div>
+                    <div><input type='text' value={props.data.foutText} onChange={(e) => {
+                        const data = {...props.data};
+                        data.foutText = e.currentTarget.value;
+                        props.onChange(data);
+                    }}/></div>
+                    <For each={props.data.words}>
+                        {(word, wordIndex) => (<>
+                            <div><input type='text' value={word.word} onChange={(e) => {
+                                const data = {...props.data}
+                                data.words[wordIndex()].word = e.currentTarget.value;
+                                props.onChange(data);
+                            }}/></div>
+                            <div><input type='color' value={word.hex} onChange={(e) => {
+                                const data = {...props.data};
+                                data.words[wordIndex()].hex = e.currentTarget.value;
+                                props.onChange(data);
+                            }}/></div>
+                            <div><input type='text' value={word.text} onChange={(e) => {
+                                const data = {...props.data};
+                                data.words[wordIndex()].text = e.currentTarget.value;
+                                props.onChange(data);
+                            }}/></div>
+                        </>)}
+                    </For>
+                </div>
+            </div>
         </div>
     )
 }
 
 const App: Component = () => {
     const [value, setValue] = createSignal('')
-    const [words, setWords] = createSignal<WordToScreenData>({
-        words: [
-            {
-                word: 'Banaan',
-                backgroundClass: '',
-                text: 'Geel',
-            },
-            {
-                word: 'Aardappel',
-                backgroundClass: '',
-                text: 'Rood',
-            },
-            {
-                word: 'Limoen',
-                backgroundClass: '',
-                text: 'Groen',
-            },
-        ],
-        foutBackgroundClass: '',
-        foutText: '-€50',
-    })
+    const settings: WordToScreenData = JSON.parse(localStorage.getItem('words') || '{"words":[]}')
+    const [words, setWords] = createSignal<WordToScreenData>(settings)
     const [screens, setScreens] = createSignal<ResultScreenData[]>()
+    const [showSettings, setShowSettings] = createSignal(false);
 
     const valueRef = createMemo(() => value())
+
+    createEffect(() => {
+        localStorage.setItem('words', JSON.stringify(words()));
+    })
 
     return (
         <div class={styles.app}>
@@ -103,22 +144,33 @@ const App: Component = () => {
             <TextInput
                 value={valueRef()}
                 onInputChange={(e) => {
-                    setValue(e.target.value)
+                    setValue(e.target.value || '')
                 }}
                 onSubmit={(val) => {
                     let result = false
+
+                    if (val == '') {
+                        return;
+                    }
+                    
                     setValue('')
 
+                    if (val == '!SETTINGS!') {
+                        setShowSettings(true);
+                        return;
+                    }
+
                     for (const i in words().words) {
-                        const word = words().words[i]
+                        const word = words().words[i];
                         if (val.toLowerCase() == word.word.toLowerCase()) {
-                            setScreens([{ text: word.text, backgroundClass: word.backgroundClass }])
+                            setScreens([{ text: word.text, hex: word.hex }])
                             result = true
                         }
                     }
 
                     if (!result) {
-                        setScreens([{ text: words().foutText, backgroundClass: words().foutBackgroundClass }])
+                        console.log(words().foutHex)
+                        setScreens([{ text: words().foutText, hex: words().foutHex }])
                     }
                 }}
             />
@@ -133,7 +185,9 @@ const App: Component = () => {
                     />
                 )}
             </For>
-            {/* <Settings></Settings> */}
+            <Show when={showSettings()}>
+                <Settings data={words()} onChange={(words) => setWords(words)} onClose={() => setShowSettings(false)}></Settings>
+            </Show>
         </div>
     )
 }
