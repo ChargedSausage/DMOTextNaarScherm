37390692571a0c60fe5730db078f6dc83841f29b
ThomasGerr <mail@thomasgerrits.nl>
2022-07-07 01:10:51 +0200

    Added fullscreen support using '!FS' commando

--- src/App.tsx
+++ src/App.tsx
@@ -43,6 +43,7 @@ const App: Component = () => {
         <div
             style={`--screen-height: ${windowDimensions().height}; --screen-width: ${windowDimensions().width};`}
             class={styles.app}
+            id='APP'
         >
             <TextInput
                 value={valueRef()}
@@ -58,8 +59,20 @@ const App: Component = () => {
 
                     setValue('')
 
-                    if (val == '!SETTINGS!') {
+                    if (val == '!SETTINGS') {
                         setShowSettings(true)
+                        return
+                    }
+
+                    if (val == '!FS') {
+                        const component = document.getElementById('APP')
+                        if (document.fullscreenElement) {
+                            document.exitFullscreen()
+                            return
+                        }
+                        if (component?.requestFullscreen) {
+                            component.requestFullscreen()
+                        }
                         return
                     }
 
