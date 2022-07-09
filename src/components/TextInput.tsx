401211b885d21a6ec875f767c2c16cec43be59ab
chargedsausage <chargedsausage@gmail.com>
2022-07-09 12:46:36 +0200

    terug van demonstratie

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -35,7 +35,7 @@ export const TextInput: Component<{
                     setWord('')
                 }}
             >
-                Gerard
+                Probeer
             </button>
         </div>
     )
